37ddba26576678e8fd3fb9a3f4d8ebe617a12678
Илья <povarejkee@yandex.ru>
2020-09-20 18:52:37 +0300

    Обновил проект готовым решением

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
@@ -57,12 +57,8 @@ module.exports = {
     }),
     new CopyPlugin([
       {
-        patterns: [
-          {
-            from: path.resolve(__dirname, 'src/favicon.ico'),
-            to: path.resolve(__dirname, 'dist')
-          }
-        ]
+        from: path.resolve(__dirname, 'src/favicon.ico'),
+        to: path.resolve(__dirname, 'dist')
       }
     ]),
     new MiniCssExtractPlugin({
